09c9b51e2b2eac16fa4d4e8455a5b4876a29c67b
maru <github@s106.org>
2017-10-06 23:28:11 +0200

    Final version of configure

--- configure/run-tests.sh
+++ configure/run-tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PREFIX_DIR=/home/maru/dev/http2/usr/local
+PREFIX_DIR=$HOME/dev/http2/usr/local
 
 reset
 autoconf || exit 1
@@ -21,7 +21,7 @@ function run_test_check
   if [ "$output" -eq 1 ]; then
     echo -e "\e[1m\e[32mOK!\e[0m"
   else
-    echo -e "\e[1m\e[31mFAILED\e[0m" $output
+    echo -e "\e[1m\e[31mFAILED\e[0m"
   fi
   if [ "$VERBOSE" -eq 1 ]; then
     ./configure -n --prefix=${PREFIX_DIR} ${1} 2>&1 | grep "$2"
@@ -31,14 +31,15 @@ function run_test_check
 
 GREP_PATTERN="HTTP/2\|http2\|HTTP2"
 
+# This test can fail, because libnghttp2 is installed in the system
 CHECK_PATTERN="^configure: error: cannot find usable libnghttp2 at specified prefix /path$"
 run_test_check "--enable-http2 --with-nghttp2=/path"     "$GREP_PATTERN" "$CHECK_PATTERN"
 
 CHECK_PATTERN="HTTP2 support:     yes (using libnghttp2)"
 run_test_check "--enable-http2 --with-nghttp2=${PREFIX_DIR}" "$GREP_PATTERN" "$CHECK_PATTERN"
 
-run_test "--enable-http2" "$GREP_PATTERN"
-run_test "--enable-http2 --with-nghttp2" "$GREP_PATTERN"
+run_test_check "--enable-http2"                              "$GREP_PATTERN" "$CHECK_PATTERN"
+run_test_check "--enable-http2 --with-nghttp2"               "$GREP_PATTERN" "$CHECK_PATTERN"
 
 CHECK_PATTERN="^configure: error: HTTP2 support cannot be enabled without libnghttp2.$"
 run_test_check "--enable-http2 --with-nghttp2=no"     "$GREP_PATTERN" "$CHECK_PATTERN"
