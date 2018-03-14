74e125671afe9990f71a0ac9e6c097f12aafe2f7
maru <github@s106.org>
2018-03-14 16:36:24 +0100

    HTTP2 MHD examples

--- mhd-examples/minimal_example.c
+++ mhd-examples/minimal_example.c
@@ -25,9 +25,11 @@
 
 #include "platform.h"
 #include <microhttpd.h>
+#ifdef HTTP2_SUPPORT
 #include <nghttp2/nghttp2.h>
+#endif /* HTTP2_SUPPORT */
 
-#define PAGE "<html><head><title>libmicrohttpd demo</title></head><body>libmicrohttpd demo</body></html>\n"
+#define PAGE "<html><head><title>libmicrohttpd demo</title></head><body>libmicrohttpd demo</body></html>"
 
 static int
 ahc_echo (void *cls,
@@ -58,7 +60,14 @@ ahc_echo (void *cls,
   response = MHD_create_response_from_buffer (strlen (me),
 					      (void *) me,
 					      MHD_RESPMEM_PERSISTENT);
+
+  /* Add custom header */
   MHD_add_response_header(response, "header-test", "my value");
+
+  /* Add custom trailer */
+  MHD_add_response_header(response, "Trailer", "foo");
+  MHD_add_response_footer(response, "foo", "bar");
+
   ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
   MHD_destroy_response (response);
   return ret;
@@ -68,31 +77,49 @@ int
 main (int argc, char *const *argv)
 {
   struct MHD_Daemon *d;
+  int use_http2 = 0;
+  uint16_t port;
 
-  if (argc != 2)
+  switch (argc)
     {
-      printf ("%s PORT\n", argv[0]);
+    case 2:
+      port = atoi (argv[1]);
+      break;
+    case 3:
+      if (strcmp(argv[1], "-h2") == 0)
+        {
+          use_http2 = MHD_USE_HTTP2;
+          port = atoi (argv[2]);
+          break;
+        }
+    default:
+      printf ("%s [-h2] PORT\n", argv[0]);
       return 1;
     }
 
+#ifdef HTTP2_SUPPORT
   /* Default HTTP2 server settings */
   nghttp2_settings_entry settings[3];
   int slen = 0;
   settings[slen].settings_id = NGHTTP2_SETTINGS_MAX_CONCURRENT_STREAMS;
   settings[slen].value = 100;
   ++slen;
+#endif /* HTTP2_SUPPORT */
 
-  d = MHD_start_daemon (/* MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG, */
-                        MHD_USE_AUTO | MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG | MHD_USE_HTTP2,
+  d = MHD_start_daemon (use_http2 |
+                        /* MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG, */
+                        MHD_USE_AUTO | MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG,
                         /* MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG | MHD_USE_POLL, */
-                      /* MHD_USE_THREAD_PER_CONNECTION | MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG | MHD_USE_POLL, */
-                      /* MHD_USE_THREAD_PER_CONNECTION | MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG, */
-                      atoi (argv[1]),
-                      NULL, NULL, &ahc_echo, PAGE,
-			MHD_OPTION_CONNECTION_TIMEOUT, (unsigned int) 10,
-			MHD_OPTION_STRICT_FOR_CLIENT, (int) 1,
-			MHD_OPTION_H2_SETTINGS, slen, settings,
-			MHD_OPTION_END);
+                        /* MHD_USE_THREAD_PER_CONNECTION | MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG | MHD_USE_POLL, */
+                        /* MHD_USE_THREAD_PER_CONNECTION | MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG, */
+                        port,
+                        NULL, NULL, &ahc_echo, PAGE,
+                        MHD_OPTION_CONNECTION_TIMEOUT, (unsigned int) 10,
+                        MHD_OPTION_STRICT_FOR_CLIENT, (int) 1,
+#ifdef HTTP2_SUPPORT
+                        MHD_OPTION_H2_SETTINGS, slen, settings,
+#endif /* HTTP2_SUPPORT */
+                        MHD_OPTION_END);
   if (d == NULL)
     return 1;
   (void) getc (stdin);
